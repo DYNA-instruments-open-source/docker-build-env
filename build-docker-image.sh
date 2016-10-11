3df8fd0b55e43c04953f767e7c0cde7242c4ced2
JoLuxer <linuxer@quantentunnel.de>
2016-10-11 14:50:51 +0200

    Docker image for build root environments and native builds.

---
+++ build-docker-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker_dir=$(dirname $(readlink -f $0))
+
+test -r $docker_dir/Dockerfile || { echo "missing Dockerfile beside $0"; exit -1; }
+
+test -r $docker_dir/.dockerignore || {
+cat >$docker_dir/.dockerignore <<EOM
+$(basename $readlink -f $0))
+EOM
+}
+
+exec docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=${https_proxy:-$http_proxy} --tag joluxer/build-env:centos7 $docker_dir
