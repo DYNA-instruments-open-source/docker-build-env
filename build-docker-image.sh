78d020c3660dbec63a4ccf74815c54f8e1b9dba5
JoLuxer <linuxer@quantentunnel.de>
2016-10-11 16:19:24 +0200

    Script for local build.

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -6,8 +6,9 @@ test -r $docker_dir/Dockerfile || { echo "missing Dockerfile beside $0"; exit -1
 
 test -r $docker_dir/.dockerignore || {
 cat >$docker_dir/.dockerignore <<EOM
-$(basename $readlink -f $0))
+$(basename $(readlink -f $0))
 EOM
+
 }
 
 exec docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=${https_proxy:-$http_proxy} --tag dynainstrumentsoss/build-env:centos7 $docker_dir
